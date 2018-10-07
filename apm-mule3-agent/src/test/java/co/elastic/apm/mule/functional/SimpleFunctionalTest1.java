d26bedad9146f8646d6e68771fefec061cf34285
axolotl <axolotl@172.19.248.113>
2018-10-07 22:11:03 +1100

    Validating transaction name

--- apm-mule3-agent/src/test/java/co/elastic/apm/mule/functional/SimpleFunctionalTest1.java
+++ apm-mule3-agent/src/test/java/co/elastic/apm/mule/functional/SimpleFunctionalTest1.java
@@ -1,7 +1,11 @@
 package co.elastic.apm.mule.functional;
 
+import static org.junit.Assert.assertEquals;
+
 import org.junit.Test;
 import org.mockito.Mockito;
+import org.mockito.invocation.InvocationOnMock;
+import org.mockito.stubbing.Answer;
 import org.mule.tck.junit4.FunctionalTestCase;
 
 import co.elastic.apm.bci.ElasticApmAgent;
@@ -12,10 +16,20 @@ import net.bytebuddy.agent.ByteBuddyAgent;
 
 public class SimpleFunctionalTest1 extends FunctionalTestCase {
 
+	private Transaction tx;
+
 	@Test
 	public void simpleFlowSendsOneTransaction() throws Exception {
 
 		Reporter reporter = Mockito.mock(Reporter.class);
+
+		Mockito.doAnswer(new Answer<Transaction>() {
+			@Override
+			public Transaction answer(InvocationOnMock invocation) throws Throwable {
+				tx = invocation.getArgumentAt(0, Transaction.class);
+				return null;
+			}
+		}).when(reporter).report(Mockito.any(Transaction.class));
 
 		ElasticApmAgent.initInstrumentation(new ElasticApmTracerBuilder().reporter(reporter).build(),
 				ByteBuddyAgent.install());
@@ -23,6 +37,8 @@ public class SimpleFunctionalTest1 extends FunctionalTestCase {
 		runFlow("test1Flow");
 
 		Mockito.verify(reporter, Mockito.times(1)).report(Mockito.any(Transaction.class));
+
+		assertEquals("test1Flow", tx.getName().toString());
 	}
 
 	@Override
